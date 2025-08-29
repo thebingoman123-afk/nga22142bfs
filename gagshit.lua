f25aaa4d477d6f8709c28e572e1d0f303c7d5937
thebingoman123-afk <thebingoman123@gmail.com>
2025-08-29 21:12:56 +0100

    Update gagshit.lua

--- gagshit.lua
+++ gagshit.lua
@@ -13,13 +13,13 @@ local StartRolls = false
 
 local function Roll()
     local args = {
-	    "ub_sun",
+	    "ub_bee",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
 	task.wait(0.5)
 	 local args = {
-	    "ub_su",
+	    "ub_bee",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
